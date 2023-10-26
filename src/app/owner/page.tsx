1973de666f84adfb2a87de44754c4d594093ee77
congcuong095 <congcuong095@gmail.com>
2023-10-26 14:25:45 +0700

    update color

--- src/app/owner/page.tsx
+++ src/app/owner/page.tsx
@@ -1,8 +1,28 @@
 "use client";
 import React from "react";
 import Report from "@/component/Report";
+import { ConfigProvider } from "antd";
 
 const App: React.FC = () => (
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: "#3aa3d9",
+        borderRadius: 6,
+        colorBorderSecondary: "#d9d9d9",
+      },
+      components: {
+        Button: {
+          colorPrimary: "#3aa3d9",
+        },
+        Table: {
+          headerBg: "#3aa3d9",
+          headerColor: "white",
+          rowHoverBg: "#3aa3d91a",
+        },
+      },
+    }}
+  >
   <div
     style={{
       padding: "40px",
@@ -10,6 +30,7 @@ const App: React.FC = () => (
   >
     <Report />
   </div>
+    </ConfigProvider>
 );
 
 export default App;
